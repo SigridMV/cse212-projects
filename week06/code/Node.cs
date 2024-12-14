32a78b775b7200c74e0755d68dcfddd407ef2cb1
Sigrid Muller <sigrid.muller.vidal@gmail.com>
2024-12-14 11:14:45 -0300

    ok

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -13,6 +13,8 @@ public class Node
     {
         // TODO Start Problem 1
 
+        if (value == Data) return;
+
         if (value < Data)
         {
             // Insert to the left
@@ -34,12 +36,23 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        if (value == Data) return true;
+
+        if (value < Data)
+        {
+            return Left?.Contains(value) ?? false;
+        }
+        else
+        {
+            return Right?.Contains(value) ?? false;
+        }
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+        return Math.Max(leftHeight, rightHeight) + 1;
     }
 }
